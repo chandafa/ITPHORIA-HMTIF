9e264586666824abb12f76d8f93678f6729e3b67
chandafa <chankirana722@gmail.com>
2024-05-14 13:44:21 +0700

    clear

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 19, 2024 13:00:10").getTime();
+var countDownDate = new Date("May 25, 2024 13:00:10").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
